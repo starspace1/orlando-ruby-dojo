5a5e43de55d0d21acc0f062ff1f2f3f8ea3106a4
Leslie Brown <lkb@alum.mit.edu>
2016-09-23 00:08:36 +0000

    Adding tests for 4 through 10

--- roman.rb
+++ roman.rb
@@ -1,10 +1,16 @@
 class Roman
 
   def convert(num)
-    if num < 5
+    if num < 4
       'I' * num
-    elsif num==5
-      'V'
+    elsif num < 6
+      'I' * (5 - num) + 'V'
+    elsif num < 9
+      'V' +  'I' * (num - 5)
+    elsif num == 9
+      'IX'
+    else
+      'X'
     end
   end
 
--- roman_test.rb
+++ roman_test.rb
@@ -32,13 +32,40 @@ class TestRoman < Minitest::Test
     assert_equal 'I', @roman.convert(1)
   end
 
-  def test_roman_converts_5_to_V
-    assert_equal 'V', @roman.convert(5)
+  def test_roman_converts_2_to_II
+    assert_equal 'II', @roman.convert(2)
   end
 
   def test_roman_converts_3_to_III
     assert_equal 'III', @roman.convert(3)
   end
 
-
+  def test_roman_converts_4_to_IV
+    assert_equal 'IV', @roman.convert(4)
+  end  
+  
+  def test_roman_converts_5_to_V
+    assert_equal 'V', @roman.convert(5)
+  end
+  
+  def test_roman_converts_6_to_VI
+    assert_equal 'VI', @roman.convert(6)
+  end
+    
+  def test_roman_converts_7_to_VII
+    assert_equal 'VII', @roman.convert(7)
+  end
+  
+  def test_roman_converts_8_to_VIII
+    assert_equal 'VIII', @roman.convert(8)
+  end
+  
+  def test_roman_converts_9_to_IX
+    assert_equal 'IX', @roman.convert(9)
+  end
+  
+  def test_roman_converts_10_to_X
+    assert_equal 'X', @roman.convert(10)
+  end
+  
 end
